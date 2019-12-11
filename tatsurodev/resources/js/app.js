65b4aec45f1461b3eb40dfb5e37560f97d8666cf
Tatsuro <contacts@tatsuro.dev>
2019-12-12 06:14:58 +0900

    npmでfontawesome install
    npm install @fortawesome/fontawesome --save-dev
    npm install @fortawesome/fontawesome-free-regular --save-dev
    npm install @fortawesome/fontawesome-free-solid --save-dev
    npm install @fortawesome/fontawesome-free-brands --save-dev

--- tatsurodev/resources/js/app.js
+++ tatsurodev/resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require("./bootstrap")
+
+import fontawesome from "@fortawesome/fontawesome"
+import regular from "@fortawesome/fontawesome-free-solid"
+import solid from "@fortawesome/fontawesome-free-regular"
+import brands from "@fortawesome/fontawesome-free-brands"
+
+fontawesome.library.add(regular)
+fontawesome.library.add(solid)
+fontawesome.library.add(brands)
