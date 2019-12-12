e81837eb7fe98ec19d8e81921419271985e10b05
Tatsuro <contacts@tatsuro.dev>
2019-12-13 06:44:29 +0900

    全skillをviewに表示

--- tatsurodev/resources/views/components/skill-bar.blade.php
+++ tatsurodev/resources/views/components/skill-bar.blade.php
@@ -1,9 +1,9 @@
-<div>
+<div class="col-md-6">
     <div class="d-flex align-items-center">
         <h4 class="p-2">{{ $skill }}</h4>
         <span class="p-2 small text-muted">{{ $slot }}</span>
     </div>
     <div class="progress mb-3">
-        <div class="progress-bar progress-bar-striped bg-{{ $color }}" style="width:{{ $level }}%"></div>
+        <div class="progress-bar progress-bar-striped bg-{{ $color }}" style="width:{{ $level * 20 }}%"></div>
     </div>
 </div>
