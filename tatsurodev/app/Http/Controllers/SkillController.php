ae9aeb14ed5cf4c3b46c545e98f633520fcfb4de
Tatsuro <contacts@tatsuro.dev>
2019-12-15 22:30:17 +0900

    SkillController変数、コメント修正

--- tatsurodev/app/Http/Controllers/SkillController.php
+++ tatsurodev/app/Http/Controllers/SkillController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Skills\StoreRequest;
 use App\Skill;
 use App\SkillLevel;
-use Illuminate\Http\Request;
 
 class SkillController extends Controller
 {
@@ -13,26 +12,15 @@ class SkillController extends Controller
     {
         $this->middleware(['auth']);
     }
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(StoreRequest $request)
     {
-        $validated = $request->validated();
-        $validated['skill_level_id'] = SkillLevel::where('level', $validated['skill_level_id'])->first()->id;
-        $skill = Skill::create($validated);
+        $validatedData = $request->validated();
+        $validatedData['skill_level_id'] = SkillLevel::where('level', $validatedData['skill_level_id'])->first()->id;
+        $skill = Skill::create($validatedData);
         return redirect()->route('home')->withStatus('新たなスキルが作成されました');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Skill  $skill
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Skill $skill)
     {
         return view('homes.index', [
@@ -40,26 +28,14 @@ class SkillController extends Controller
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Skill  $skill
-     * @return \Illuminate\Http\Response
-     */
     public function update(StoreRequest $request, Skill $skill)
     {
         $validatedData = $request->validated();
+        $validatedData['skill_level_id'] = SkillLevel::where('level', $validatedData['skill_level_id'])->first()->id;
         $skill->fill($validatedData)->save();
         return redirect()->route('home')->withStatus('スキルが更新されました');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Skill  $skill
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Skill $skill)
     {
         $skill->delete();
