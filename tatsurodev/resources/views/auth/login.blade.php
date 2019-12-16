142087ff0e7ad36a0816ffaaf82f85e6432c7f6e
Tatsuro <contacts@tatsuro.dev>
2019-12-16 19:48:20 +0900

    login用link、formを作成

--- tatsurodev/resources/views/auth/login.blade.php
+++ tatsurodev/resources/views/auth/login.blade.php
@@ -1,73 +1,44 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+<div>
+    <div class="card card-body bg-dark text-white py-5">
+        <h2 class="text-capitalize">{{ __('admin') . ' ' . __('login') }}</h2>
+    </div>
+</div>
+<div class="card card-body py-5">
+    <form action="{{ route('login') }}" method="post">
+        @csrf
+        <div class="form-group">
+            <div class="input-group input-group-lg">
+                <div class="input-group-prepend">
+                    <span class="input-group-text bg-dark text-white">
+                        <i class="fas fa-envelope"></i>
+                    </span>
                 </div>
+                <input type="text" name="email" class="form-control bg-dark text-white{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="Email" value="{{ old('email') }}" />
+                @if($errors->has('email'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+                @endif
             </div>
         </div>
-    </div>
+        <div class="form-group">
+            <div class="input-group input-group-lg">
+                <div class="input-group-prepend">
+                    <span class="input-group-text bg-dark text-white">
+                        <i class="fas fa-key"></i>
+                    </span>
+                </div>
+                <input type="password" name="password" class="form-control bg-dark text-white{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="Password" />
+                @if($errors->has('password'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+                @endif
+            </div>
+        </div>
+        <input type="submit" value="{{ __('login') }}" class="btn btn-dark btn-block btn-lg text-capitalize" />
+    </form>
 </div>
 @endsection
