32667fbbc562913069b655578c02551ad1f4bbee
Tatsuro <contacts@tatsuro.dev>
2019-12-16 19:51:00 +0900

    login, logout処理を成功時にtoastrで通知が出るようにカスタマイズ
    defaultのAuthenticatesUsers traitsをoverrideしている

--- tatsurodev/resources/lang/en/messages.php
+++ tatsurodev/resources/lang/en/messages.php
@@ -3,4 +3,6 @@
 return [
     'welcome' => "I'm making efforts to become Full Stack Engineer.<br>I owe every my coding skill to tech masters in Udemy. Thank you, Senseis!",
     'change_locale' => ':language was selected.',
+    'logged_in' => 'You were logged in successfully.',
+    'logged_out' => 'You were logged out successfully.',
 ];
