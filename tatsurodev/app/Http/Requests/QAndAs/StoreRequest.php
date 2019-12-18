26fcc469b90ad46a03943bba77d3a11f051f400b
Tatsuro <contacts@tatsuro.dev>
2019-12-19 01:08:40 +0900

    QAndAControllerのcrud完成

---
+++ tatsurodev/app/Http/Requests/QAndAs/StoreRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\QAndAs;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    protected $errorBag = 'qanda';
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'question' => 'required|string|max:255',
+            'answer' => 'required|string',
+            'menu_id' => 'required|integer',
+        ];
+    }
+}
